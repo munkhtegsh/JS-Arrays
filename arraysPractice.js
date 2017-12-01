83492931181f3674c86a8e10915fe724fee33005
Munkhtegsh Munkhbat <munkhtegsh@munkhtegshsimac.attlocal.net>
2017-12-01 11:48:24 -0800

    The divider and finder functions will pass.

--- arraysPractice.js
+++ arraysPractice.js
@@ -2,11 +2,11 @@
 //in the console.
 
 
-var arr = [10,20,30];
+var arr = [10, 20, 30];
 //Create a function named 'first' that is given 'arr' as the argument
 // and  returns the first item in the given array.
 
-  //Code Here
+//Code Here
 var first = (arr) => {
   return arr[0];
 }
@@ -15,11 +15,13 @@ var first = (arr) => {
 
 
 
-var arr = [40,50,60];
+var arr = [40, 50, 60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item in the given array.
-var last = (arr) => { return arr[arr.length - 1] };
+var last = (arr) => {
+  return arr[arr.length - 1]
+};
 
-  //Code Here
+//Code Here
 
 
 //Next Problem
@@ -28,7 +30,7 @@ var last = (arr) => { return arr[arr.length - 1] };
 var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
-  //Code Here
+//Code Here
 var looper = (arr) => {
   arr.forEach((e) => alert(e));
 }
@@ -42,34 +44,34 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 //it's only argument and loops through the given array backwards 
 //alerting every item in the array starting at the end.
 
-  //Code Here
+//Code Here
 var reversedLooper = (arr) => {
   for (var i = arr.length - 1; i >= 0; i--) {
     alert(arr[i]);
-  }  
+  }
 }
 
 //Next Problem
 
 
-var nums = [1,2,3,6,22,98,45,23,22,12];
+var nums = [1, 2, 3, 6, 22, 98, 45, 23, 22, 12];
 //Write a function named evenFinder that is given nums as it's only 
 //argument and removes all values that aren't even from the given array.
 
-  //Code Here
-var evenFinder = (arr) => { 
+//Code Here
+var evenFinder = (arr) => {
   var newArr = [];
   arr.forEach((e) => {
     if (e % 2 === 0) {
-      newArr.push(e)
+      newArr.push(e);
     }
-  })
+  });
   return newArr;
- }
+}
 
 
 
-  
+
 
 
 
@@ -87,28 +89,54 @@ var evenFinder = (arr) => {
 //Next problem
 
 
-var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
+var numbersArray = [1, 2, 34, 54, 55, 34, 32, 11, 19, 17, 54, 66, 13];
 //Write a function called divider that is given one argument, numbersArray.
-//Have divider return an Array with the first item in the array being the evens array (all the even values from numbersArray) and the second item in the Array being the odds array (all the odd values from numbersArray).
+//Have divider return an Array with the first item in the array being the 
+//evens array (all the even values from numbersArray) and the second item in 
+//the Array being the odds array (all the odd values from numbersArray).
 
 
 
-  //Code Here
-
+//Code Here
+var divider = (arr) => {
+  var even = [];
+  var odd = [];
+  arr.forEach((e) => {
+    if (e % 2 === 0) {
+      even.push(e);
+    } else {
+      odd.push(e);
+    }
+  });
+  return [even, odd];
+}
 
 //Next Problem
 
 
-var getRandomArbitrary = function() {
+var getRandomArbitrary = function () {
   return Math.floor(Math.random() * 30);
 };
 // var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
-//Above you're given a function that will return a random number between 0 and 30.  There is also a commented out array full of numbers to help you visualize what your function will be receiving.
+//Above you're given a function that will return a random number between 0 
+//and 30.  There is also a commented out array full of numbers to help you
+// visualize what your function will be receiving.
 
-// Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
+// Your job is to write a function named finder that will get a random 
+//number (by invoking getRandomArbitrary), then loop through the 
+//array (that will be passed in as a parameter) to see if that random number 
+//is in the array. If it is, return true, if it's not, return false
 
-  //Code Here
-
+//Code Here
+var finder = (arr) => {
+  var randomNum = getRandomArbitrary();
+  if (arr.indexOf(randomNum) > -1 ) {
+    return true;
+  } else  {
+    return false;
+  }
+ 
+}
 
 
 
@@ -131,7 +159,7 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
   In both the removeItem function and the addItem function, you will also need to check for valid aurguments. Specrunner will try to call your functions without passing in valid aurguments. When this happens, you will need to respond by returning an empty array.
 */
 
-  //Code Here
+//Code Here
 
 //removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 //addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
@@ -144,7 +172,7 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 
 //Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
 
-  //Code Here
+//Code Here
 
 
 
@@ -155,7 +183,7 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
 //Write a function called addTen that is given 'numbers' as it's only argument and returns a new
 //array after adding ten to each item in numbers. *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
 
-  //Code Here
+//Code Here
 
 
 
@@ -167,16 +195,16 @@ var num1 = Math.floor(Math.random() * 30);
 var num2 = Math.floor(Math.random() * 30);
 var arr1 = [];
 var arr2 = [];
-for(var i = 0; i < num1; i++){
+for (var i = 0; i < num1; i++) {
   arr1.push(i);
 }
-for(var i = 0; i < num2; i++){
+for (var i = 0; i < num2; i++) {
   arr2.push(i);
 }
 //Above is some code that adds a random number of values to both arr1 and arr2.
 //Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. Return the array which is longest.
 
-  //Code Here
+//Code Here
 
 
 /*
@@ -189,7 +217,7 @@ Your 'both' function will be given two arguments, arr1 and arr2 (from the previo
 Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
 */
 
-  //Code Here
+//Code Here
 
 
 
@@ -202,39 +230,39 @@ Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
 var devMountainEmployees = [];
 
 var tyler = {
-    name: 'Tyler',
-    position: 'Lead Instructor/Engineer',
-    spiritAnimal: 'Honey Badger'
+  name: 'Tyler',
+  position: 'Lead Instructor/Engineer',
+  spiritAnimal: 'Honey Badger'
 };
 
 var cahlan = {
-    name: 'Cahlan',
-    position: 'CEO',
-    spiritAnimal: 'butterfly'
+  name: 'Cahlan',
+  position: 'CEO',
+  spiritAnimal: 'butterfly'
 };
 
 var ryan = {
-    name: 'Ryan',
-    position: 'Marketing',
-    spiritAnimal: 'fox'
+  name: 'Ryan',
+  position: 'Marketing',
+  spiritAnimal: 'fox'
 };
 
 var colt = {
-    name: 'Colt',
-    position: 'Everything really',
-    spiritAnimal: 'Young Male Horse'
+  name: 'Colt',
+  position: 'Everything really',
+  spiritAnimal: 'Young Male Horse'
 };
 
 /*Above you're given an empty array with four objects. Fill the devMountainEmployees
 array with those four objects. After that console.log the length of the Array and make
 sure that it's equal to 4. */
 
-  //Code Here
+//Code Here
 
 /*Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
 Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
 
-  //Code Here
+//Code Here
 
 
 
@@ -247,17 +275,17 @@ of Data is to have an Array full of objects. */
 
 //Create an empty array called users.
 
-  //Code Here
+//Code Here
 
 /*Now add three user objects to your users array. Each user object should contain the
 following properties. name, email, password, username.*/
 
 //include this as one of the objects in your array.
 var user1 = {
-    name: 'Tyler McGinnis',
-    email: 'tylermcginnis33@gmail.com',
-    password: 'iLoveJavaScript',
-    username: 'infiniateLoop'
+  name: 'Tyler McGinnis',
+  email: 'tylermcginnis33@gmail.com',
+  password: 'iLoveJavaScript',
+  username: 'infiniateLoop'
 };
 
 //Your Code Here
@@ -270,6 +298,6 @@ and those objects contain properties about the specific person you follow.*/
 objects until you find Tyler's account (use tylermcginnis33@gmail.com to find him).
 Once you find the particular index he's located in, delete him from the array.*/
 
-  //Code Here
+//Code Here
 
 //The activity we just did is very much how data works in 'the real world'.
